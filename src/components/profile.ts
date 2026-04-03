adac698d647136776a7d0237a3dfbc23fd75c5ad
Federico 'Morg' Pareschi <morgawr@gmail.com>
2026-04-03 19:33:15 +0900

    Addressed sonar findings

--- src/components/profile.ts
+++ src/components/profile.ts
@@ -219,7 +219,7 @@ const ENABLE_SYNC_AUTH_TIMEOUT_ERROR =
 function formatBytes(bytes: number, decimals = 1): string {
     if (bytes === 0) return '0 Bytes';
     const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals;
+    const dm = Math.max(0, decimals);
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return `${Number.parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
